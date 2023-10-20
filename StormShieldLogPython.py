6b9dbadbf1a49b35a5cecf4ad9600c4e7dfac0fd
SyRoxS1 <test.teste44momo@gmail.com>
2023-10-20 09:48:15 +0200

    Clarity updates

--- StormShieldLogPython.py
+++ StormShieldLogPython.py
@@ -1,20 +1,26 @@
 import requests
 import pyotp
 
-otpauth_uri = "otpauth://"
-url = 'https://10.15.254.254/auth/plain.html'
+IP = 'PUT IP OR NAME'
+username = 'username'
+password = 'password'
+cookiec = 'YES' #YES OR NO if you want to save cookie : YES else : NO
+otpauth_uri = "PUT THE otpauth path here starting with : otpauth://"
+
+url = 'https://'+IP+'/auth/plain.html'
 totp = pyotp.parse_uri(otpauth_uri)
 otp_code = totp.now()
+
 headers = {
-    'Host': '10.15.254.254',
+    'Host': IP,
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Accept-Language': 'en-US,en;q=0.5',
     'Accept-Encoding': 'gzip, deflate',
-    'Referer': 'https://10.15.254.254/auth/',
+    'Referer': 'https://'+IP+'/auth/',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Content-Length': '100',
-    'Origin': 'https://10.15.254.254',
+    'Origin': 'https://'+IP,
     'Upgrade-Insecure-Requests': '1',
     'Sec-Fetch-Dest': 'document',
     'Sec-Fetch-Mode': 'navigate',
@@ -26,16 +32,15 @@ headers = {
 data = {
     'url': '',
     'logout': '',
-    'uid': 'username',
+    'uid': username,
     'authnum': '0',
-    'pswd': 'mdp',
+    'pswd': password,
     'totp': otp_code,
     'n_time': '240'
 }
 
 response = requests.post(url,verify=False ,headers=headers, data=data)
-
-print(response.text)
-f = open('cookie.txt','w')
-f.write(str(response.cookies))
-f.close()
+if cookiec == 'YES':
+    f = open('cookie.txt','w')
+    f.write(str(response.cookies))
+    f.close()
